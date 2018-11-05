383ba066be106cd3468eb4d58fb72d9ae50dd9b9
Akshay Guthal <akguthal@gmail.com>
2018-11-05 18:20:19 -0500

    added get menu functionality

--- server.js
+++ server.js
@@ -2,13 +2,31 @@ const express = require('express');
 const app = express();
 const wp_api = require('./wordpress-service.js');
 
+app.get('/', function(req, res){
+  res.send("Welcome to the DBK API!");
+});
+
 app.get('/posts', function (req, res) {
+  let posts = req.query.per_page;
+  let page = req.query.page;
   let category = req.query.category;
-  let posts = req.query.limit;
 
   res.setHeader('Content-Type', 'application/json');
-  wp_api.getPosts(posts, category)
-       .then(data => res.send(data))
+  wp_api.getPosts(posts, page, category)
+    .then(data => res.send(data))
+});
+
+app.get('/menu/:id', function (req, res){
+  let menu_id = req.params.id;
+
+  res.setHeader('Content-Type', 'application/json');
+  wp_api.getMenu(menu_id)
+    .then(data => res.send(data))
+});
+
+
+app.get('/ads', function (req, res){
+
 });
 
 
--- wordpress-service.js
+++ wordpress-service.js
@@ -1,15 +1,25 @@
 const fetch = require('node-fetch');
 
-const all_posts_url = "https://dbknews-wp.partner.nile.works/wp-json/posts";
+const all_posts_url = "http://52.207.216.69/wp-json/wp/v2/posts";
+const menu_url = "http://52.207.216.69/wp-json/wp-api-menus/v2/menus/"
 
-exports.getPosts = function(limitPosts, category){
+exports.getPosts = function(limitPosts, page, category){
   url = all_posts_url;
   if (typeof limitPosts != 'undefined'){
-    url += "?filter[posts_per_page]="+limitPosts;
+    url += "?per_page="+limitPosts;
+  }
+  if(typeof page != 'undefined'){
+    url += "&page="+page;
   }
   if (typeof category != 'undefined'){
-    url += "&filter[category_name]="+category;
+    url += "&categories="+category;
   }
   return fetch(url)
          .then(data => data.json());
+}
+
+exports.getMenu = function(menu_id){
+  url = menu_url+menu_id;
+  return fetch(url)
+        .then(data => data.json());
 }
