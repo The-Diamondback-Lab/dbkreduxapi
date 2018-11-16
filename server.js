b77b936b5007d5b19aafc9276e849a49fbe9edb6
Akshay Guthal <akguthal@gmail.com>
2018-11-15 19:27:21 -0500

    added endpoint to get article based on an ID

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ app.get('/', function(req, res){
   res.send("Welcome to the DBK API!");
 });
 
-app.get('/posts', function (req, res) {
-  let posts = req.query.per_page;
+app.get('/articles', function (req, res) {
+  let articles = req.query.per_page;
   let page = req.query.page;
   let category = req.query.category;
 
   res.setHeader('Content-Type', 'application/json');
-  wp_api.getPosts(posts, page, category)
+  wp_api.getArticles(articles, page, category)
     .then(data => res.send(data))
 });
+
+app.get('/articles/:articleId', function(req, res){
+  let articleId = req.params.articleId;
+
+  res.setHeader('Content-Type', 'application/json');
+  wp_api.getArticle(articleId)
+    .then(data => res.send(data))
+});
+
 
 app.get('/menu/:id', function (req, res){
   let menu_id = req.params.id;
--- wordpress-service.js
+++ wordpress-service.js
@@ -3,10 +3,10 @@ const fetch = require('node-fetch');
 const all_posts_url = "http://52.207.216.69/wp-json/wp/v2/posts";
 const menu_url = "http://52.207.216.69/wp-json/wp-api-menus/v2/menus/"
 
-exports.getPosts = function(limitPosts, page, category){
+exports.getArticles = function(limitArticles, page, category){
   url = all_posts_url;
   if (typeof limitPosts != 'undefined'){
-    url += "?per_page="+limitPosts;
+    url += "?per_page="+limitArticles;
   }
   if(typeof page != 'undefined'){
     url += "&page="+page;
@@ -17,6 +17,13 @@ exports.getPosts = function(limitPosts, page, category){
   return fetch(url)
          .then(data => data.json());
 }
+
+exports.getArticle = function(articleId){
+  url = all_posts_url+"/"+articleId;
+  return fetch(url)
+          .then(data => data.json());
+}
+
 
 exports.getMenu = function(menuName){
   return fetch(menu_url)
