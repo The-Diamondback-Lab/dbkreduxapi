d237b09a654e363eb50457aa7cbc4c1465211c5a
Akshay Guthal <akguthal@gmail.com>
2018-11-26 20:09:29 -0500

    added a crap ton of documentation

---
+++ doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "The Diamondback API",
+  "title": "DBK API",
+  "url": "localhost:8080",
+  "version": "1.0.0",
+  "description": "API to retrieve articles and other content from the DBK site.",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-27T01:07:45.056Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
