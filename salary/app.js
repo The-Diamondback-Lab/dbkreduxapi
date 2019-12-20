da92d02b987d4553db12a56089c4e8c2fcf39ad6
fishnal <vpatel14@terpmail.umd.edu>
2019-12-20 01:44:16 -0500

    indentation

--- salary/app.js
+++ salary/app.js
@@ -6,27 +6,27 @@ const router = express.Router();
 router.use(cors())
 
 router.get('/salary', (req, res) => {
-    res.redirect("https://api.dbknews.com/#tag-salary")
+  res.redirect("https://api.dbknews.com/#tag-salary")
 })
 
 router.get('/salary/year/:year', async (req, res) => {
-    let query = buildQuery(req)
-    let countQuery = buildQuery(req, "count")
+  let query = buildQuery(req)
+  let countQuery = buildQuery(req, "count")
 
-    let results = await runQuery(res, query.string, query.params)
-    let count = await runQuery(res, countQuery.string, countQuery.params)
+  let results = await runQuery(res, query.string, query.params)
+  let count = await runQuery(res, countQuery.string, countQuery.params)
 
-    sendResponse(res, 200, { data: results, count: count[0]["COUNT(*)"] })
+  sendResponse(res, 200, { data: results, count: count[0]["COUNT(*)"] })
 })
 
 router.get('/salary/years', async (req, res) => {
-    let tableQuery = {
-        string: "SHOW TABLES",
-        params: []
-    }
-    let tables = await runQuery(res, tableQuery.string, tableQuery.params)
-    tables = tables.map(t => t.Tables_in_saldbinstance.replace("Data", ''))
-    sendResponse(res, 200, { data: tables })
+  let tableQuery = {
+    string: "SHOW TABLES",
+    params: []
+  }
+  let tables = await runQuery(res, tableQuery.string, tableQuery.params)
+  tables = tables.map(t => t.Tables_in_saldbinstance.replace("Data", ''))
+  sendResponse(res, 200, { data: tables })
 })
 
 /**
@@ -37,65 +37,65 @@ router.get('/salary/years', async (req, res) => {
  *
  */
 let buildQuery = (req, type="results") => {
-    let PAGESIZE = 10
-    let COLUMNS = ['Division', "Department", "Title", "Employee", "Salary"]
-    let year = req.params.year
-    if (!year){
-        sendResponse(500, "Invalid year parameter supplied.")
-    }
-    let page = req.query.page
-    let offset = 0
-    if (page && !isNaN(page) && page >= 1){
-        offset = (page-1) * PAGESIZE
-    }
+  let PAGESIZE = 10
+  let COLUMNS = ['Division', "Department", "Title", "Employee", "Salary"]
+  let year = req.params.year
+  if (!year){
+    sendResponse(500, "Invalid year parameter supplied.")
+  }
+  let page = req.query.page
+  let offset = 0
+  if (page && !isNaN(page) && page >= 1){
+    offset = (page-1) * PAGESIZE
+  }
 
-    let queryString = `SELECT * FROM ??`
-    if (type === "count") {
-        queryString = `SELECT COUNT(*) FROM ??`
-    }
+  let queryString = `SELECT * FROM ??`
+  if (type === "count") {
+    queryString = `SELECT COUNT(*) FROM ??`
+  }
 
-    let queryParams = [year+"Data"]
+  let queryParams = [year+"Data"]
 
-    let search = req.query.search
-    if (search) {
-        queryString += ` WHERE `
-        COLUMNS.forEach((col, i) => {
-            queryString += ` ${col} LIKE ? ${i < COLUMNS.length-1 ? 'OR' : ''} `
-            queryParams.push('%'+search+'%')
-        })
-    }
+  let search = req.query.search
+  if (search) {
+    queryString += ` WHERE `
+    COLUMNS.forEach((col, i) => {
+      queryString += ` ${col} LIKE ? ${i < COLUMNS.length-1 ? 'OR' : ''} `
+      queryParams.push('%'+search+'%')
+    })
+  }
 
-    if (type === "count") {
-        return {
-            string: queryString,
-            params: queryParams
-        }
-    }
-
-    let sortby = req.query.sortby
-    if (sortby) {
-        if (sortby.toLowerCase() === 'salary'){ //For sorting salaries, we need to parse the salary value into a number
-            queryString += "ORDER BY CAST(REPLACE(REPLACE(salary,'$',''),',','') AS UNSIGNED) "
-        }
-        else{
-            queryString += ` ORDER BY REPLACE(??,' ','') ` //ignore whitespace while sorting
-            queryParams.push(sortby)
-        }
-    }
-
-    //By default, SQL orders by ASC. If the user specifies DESC, order by that instead.
-    if (req.query.order && req.query.order.toUpperCase() === 'DESC'){
-        queryString += ' DESC '
-    }
-
-    queryString += ` LIMIT ${offset}, ${PAGESIZE} `
-    queryString = queryString.trim()
-    queryString = queryString.replace(/ +/g, " ")
-
+  if (type === "count") {
     return {
-        string: queryString,
-        params: queryParams
+      string: queryString,
+      params: queryParams
     }
+  }
+
+  let sortby = req.query.sortby
+  if (sortby) {
+    if (sortby.toLowerCase() === 'salary'){ //For sorting salaries, we need to parse the salary value into a number
+      queryString += "ORDER BY CAST(REPLACE(REPLACE(salary,'$',''),',','') AS UNSIGNED) "
+    }
+    else{
+      queryString += ` ORDER BY REPLACE(??,' ','') ` //ignore whitespace while sorting
+      queryParams.push(sortby)
+    }
+  }
+
+  //By default, SQL orders by ASC. If the user specifies DESC, order by that instead.
+  if (req.query.order && req.query.order.toUpperCase() === 'DESC'){
+    queryString += ' DESC '
+  }
+
+  queryString += ` LIMIT ${offset}, ${PAGESIZE} `
+  queryString = queryString.trim()
+  queryString = queryString.replace(/ +/g, " ")
+
+  return {
+    string: queryString,
+    params: queryParams
+  }
 }
 
 /**
@@ -106,15 +106,15 @@ let buildQuery = (req, type="results") => {
  * @param {array} params - Objects to substitute into the query.
  */
 let runQuery = async function (res, query, params) {
-    let db = require('../utilities/db')
+  let db = require('../utilities/db')
 
-    try {
-        let results = await db.query(query, params)
-        return results
-    }
-    catch (err) {
-        handleError(res, err)
-    }
+  try {
+    let results = await db.query(query, params)
+    return results
+  }
+  catch (err) {
+    handleError(res, err)
+  }
 }
 
 
@@ -126,7 +126,7 @@ let runQuery = async function (res, query, params) {
  * @param {string} message - Message to return with code.
  */
 let sendResponse = (res, status, message) => {
-    res.status(status).send(message)
+  res.status(status).send(message)
 }
 
 /**
@@ -136,12 +136,12 @@ let sendResponse = (res, status, message) => {
  * @param {object} error - Error object from SQL execution.
  */
 let handleError = (res, error) => {
-    // if (error.code === 'ER_NO_SUCH_TABLE') {
-    //     sendResponse(res, 500, {message: 'Invalid Year Supplied'})
-    // }
-    // else {
-    sendResponse(res, 500, error)
-    //}
+  // if (error.code === 'ER_NO_SUCH_TABLE') {
+  //   sendResponse(res, 500, {message: 'Invalid Year Supplied'})
+  // }
+  // else {
+  sendResponse(res, 500, error)
+  //}
 }
 
 module.exports = router
