2c41abd85c2fa14d6530da813664ba8ab26d70b3
fishnal <vpatel14@terpmail.umd.edu>
2020-01-06 00:14:45 -0500

    Changed some log statements

--- src/utilities/db.js
+++ src/utilities/db.js
@@ -15,15 +15,15 @@ let pool = mysql.createPool({
 pool.getConnection((err, connection) => {
   if (err) {
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
+      console.error('[mysql] Database connection was closed.');
     }
     if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.');
+      console.error('[mysql] Database has too many connections.');
     }
     if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.');
+      console.error('[mysql] Database connection was refused.');
     } else {
-      console.error('Unexpected error');
+      console.error('[mysql] Unexpected error');
       console.error(err);
     }
   }
