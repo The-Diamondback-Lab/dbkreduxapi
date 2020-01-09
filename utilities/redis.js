accff85bbc74adf5d6ca3a4a83581c2ac5feead1
fishnal <vpatel14@terpmail.umd.edu>
2020-01-09 18:52:36 -0500

    Added NODE_ENV to disable Redis caching
    Redis caching is disabled when not in prod
    Redis cached results are ignored when not in prod

--- utilities/redis.js
+++ utilities/redis.js
@@ -6,15 +6,26 @@ let redisConf = {
   port: '6379'
 };
 
-let redisClient = redis.createClient(redisConf);
-redisClient.auth(process.env.REDISPWD);
+let redisClient = null;
 
-redisClient.on('connect', function() {
-  console.log('[redis] Client connected');
-});
+if (process.env.NODE_ENV !== 'production') {
+  redisClient = {
+    get: (_, cb) => {
+      cb(null, null)
+    },
+    setex: () => {}
+  }
+} else {
+  redisClient = redis.createClient(redisConf);
+  redisClient.auth(process.env.REDISPWD);
 
-redisClient.on("error", function (err) {
-  console.log("[redis] Error: " + err);
+  redisClient.on('connect', function() {
+    console.log('[redis] Client connected');
+  });
+
+  redisClient.on("error", function (err) {
+    console.log("[redis] Error: " + err);
 });
+}
 
 module.exports = redisClient;
